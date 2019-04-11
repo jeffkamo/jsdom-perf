64a32108ba762c968e837b80df069e2d0fc995df
Jeff Kamo <kamojeffrey@gmail.com>
2019-04-11 10:23:59 -0700

    Add experiments for JSDOM 14.0

---
+++ 14.0-no-react/index.js
@@ -0,0 +1,27 @@
+const jsdom = require('jsdom')
+const {JSDOM} = jsdom
+
+const dom = new JSDOM(
+    `<html>
+        <head>
+            <script type="text/javascript" src="file:///Users/Atsushi/Mobify/_lab/jsdom-perf/13.2-no-react/big.js"></script>
+            <script type="text/javascript">
+                var start = Date.now()
+                doTheThing(document)
+                var end = Date.now() - start
+                console.log(end)
+            </script>
+        </head>
+        <body>
+            <div id="target"></div>
+        </body>
+    </html>`,
+    {
+        contentType: 'text/html',
+        pretendToBeVisual: true,
+        runScripts: 'dangerously',
+        resources: 'usable',
+        strictSSL: true,
+        url: 'http://localhost'
+    }
+)
